cd8971ceae7f18fbb99ae97705eac2ce77691fde
Konstantin Parakhin <kostyaparahin@gmail.com>
2019-04-26 14:45:33 +0300

    Fixed code style

--- src/main/java/pages/ChatWrapper.java
+++ src/main/java/pages/ChatWrapper.java
@@ -3,20 +3,20 @@ package pages;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 
-public class ChatWrapper {
+class ChatWrapper {
 
     private static final By NAME_LOCATOR = By.className("chats_i_h");
 
     private String mName;
     private WebElement mContext;
 
-    public WebElement getContext() {
-        return mContext;
-    }
-
-    public ChatWrapper(WebElement webElement) {
+    ChatWrapper(WebElement webElement) {
         this.mContext = webElement;
         mName = mContext.findElement(NAME_LOCATOR).getText();
+    }
+
+    WebElement getContext() {
+        return mContext;
     }
 
 }
